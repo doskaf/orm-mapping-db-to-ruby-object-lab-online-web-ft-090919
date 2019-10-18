1cdbd8533713af3d36bc779793be830bf0e15804
Dominique Skaf <doskaf@yahoo.com>
2019-10-18 20:29:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,9 @@ class Student
       WHERE grade < 12
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).collect do |row|
+      self.new_from_db(row)
+    end.first
   end
 
   def self.new_from_db(row)
