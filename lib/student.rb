4a9a8db7c94e10777b71f1f55fe435b374a7088c
Dominique Skaf <doskaf@yahoo.com>
2019-10-18 20:32:58 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -31,7 +31,9 @@ class Student
       LIMIT x
     SQL
     
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.new_from_db(row)
